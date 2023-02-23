e709bc4dd290ab60225e24886a9d05b7865c826c
GrafenoTweaking+ <115122527+GrafenoTk@users.noreply.github.com>
2023-02-23 04:11:00 -0300

    Toolbox via Powershell
    💥DownloadToolboxViaPowershell

---
+++ Toolbox.ps1
@@ -0,0 +1,16 @@
+net stop bits
+sc config bits start= auto
+net start bits
+$webclient = New-Object System.Net.WebClient
+$url = "https://github.com/GrafenoTk/ToolboxG-/releases/download/G%2BToolboxUP/G+ToolboxUP.exe"
+$filepath = "C:\Windows\Temp\G+ToolboxUP.exe"
+$directory = Split-Path $filepath -Parent
+
+# Verifica se o diretório existe e, se não existir, cria ele
+if (!(Test-Path -Path $directory)) {
+    New-Item -ItemType Directory -Path $directory | Out-Null
+}
+
+$webclient.DownloadFile($url, $filepath)
+Start-Process -FilePath "cmd.exe" -ArgumentList "/c $filepath"
+exit
