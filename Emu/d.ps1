cb21b54772cc90f0bbff8b07d52930c8af8b3f49
GrafenoTweaking+ <115122527+GrafenoTk@users.noreply.github.com>
2023-02-27 03:23:29 -0300

    Update d.ps1

--- Emu/d.ps1
+++ Emu/d.ps1
@@ -1,9 +1,15 @@
-net stop bits
-sc config bits start= auto
-net start bits
-$webclient = New-Object System.Net.WebClient
+# Para o serviço Background Intelligent Transfer Service (BITS)
+Stop-Service -Name BITS
+
+# Configura o serviço BITS para ser iniciado automaticamente
+Set-Service -Name BITS -StartupType Automatic
+
+# Inicia o serviço BITS novamente
+Start-Service -Name BITS
+
+# Baixa o arquivo de script "DownloadGrafenoGaGa.bat" do GitHub e executa-o
 $url = "https://github.com/GrafenoTk/ProjetosG-/releases/download/GrafenoTw-Emulator/DownloadGrafenoGaGa.bat"
-$filepath = "C:\Grafeno"
+$filepath = "C:\Grafeno\DownloadGrafenoGaGa.bat"
 $directory = Split-Path $filepath -Parent
 
 # Verifica se o diretório existe e, se não existir, cria ele
@@ -11,6 +17,6 @@ if (!(Test-Path -Path $directory)) {
     New-Item -ItemType Directory -Path $directory | Out-Null
 }
 
+$webclient = New-Object System.Net.WebClient
 $webclient.DownloadFile($url, $filepath)
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c $filepath"
-exit
