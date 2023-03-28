5c6725aafb86f9a513daf3e26ec994bcafd95040
GrafenoTweaking+ <115122527+GrafenoTk@users.noreply.github.com>
2023-03-27 22:15:08 -0300

    Rename GTxReg.ps1 to GTxReg0x8736.ps1

--- soft/GTxReg0x8736.ps1
+++ soft/GTxReg0x8736.ps1
@@ -1,19 +1,19 @@
-# Define o diretÛrio da ·rea de trabalho
+# Define o diret√≥rio da √°rea de trabalho
 $desktopPath = [Environment]::GetFolderPath("Desktop")
 
 # Define a URL do arquivo zip
 $url = "https://github.com/GrafenoTk/ProjetosG-/releases/download/GTXREGEDIT/GT.X.REG.zip"
 
-# Define o caminho onde o arquivo zip ser· salvo
+# Define o caminho onde o arquivo zip ser√° salvo
 $zipFilePath = Join-Path $desktopPath "GTxRegedit.zip"
 
-# Define o caminho onde o arquivo ser· extraÌdo
+# Define o caminho onde o arquivo ser√° extra√≠do
 $extractPath = Join-Path $desktopPath "GTxRegedit"
 
 # Faz o download do arquivo zip
 Invoke-WebRequest -Uri $url -OutFile $zipFilePath
 
-# Extrai o conte˙do do arquivo zip para a pasta definida
+# Extrai o conte√∫do do arquivo zip para a pasta definida
 Expand-Archive -Path $zipFilePath -DestinationPath $extractPath -Force
 
 # Define o caminho completo do arquivo exe a ser executado
